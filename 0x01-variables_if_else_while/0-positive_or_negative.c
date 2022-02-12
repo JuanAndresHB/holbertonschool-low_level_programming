01a45f38bf03a06dff872b780d995e86498ed71d
JuanAndresHB <4759@holbertonschool.com>
2022-02-12 17:25:59 -0500

    pos o neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -24,48 +24,4 @@ int main(void)
 		printf("%i is negative\n", n);
 	}
 	return (0);
-} * main - This program will assign random number variable n each time executed
-* Return: void
- */
-int main(void)
-{
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%i is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%i is zero\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%i is negative\n", n);
-	}
-	return (0);
-} * main - This program will assign random number variable n each time executed
-* Return: void
- */
-int main(void)
-{
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%i is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%i is zero\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%i is negative\n", n);
-	}
-	return (0);
 }
